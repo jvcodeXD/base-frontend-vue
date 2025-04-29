16cb8744d9c621e5aa647fb57a2742e29b38fad1
Juan Visney <jvcode23@gmail.com>
2025-04-28 20:24:37 -0400

    feat: middleware proteccion de rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,55 +4,39 @@ import About from '@/views/About.vue'
 import Login from '@/views/auth/Login.vue'
 import AdminDashboard from '@/views/dashboard/AdminDashboard.vue'
 import UserDashboard from '@/views/dashboard/UserDashboard.vue'
-import { useAuthStore } from '@/store/auth'
 import NotFound from '@/views/NotFound.vue'
+import { useAuthStore } from '@/store/auth'
 
 // Definición de rutas
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-  },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/about', name: 'About', component: About },
+  { path: '/login', name: 'Login', component: Login },
   {
     path: '/admin-dashboard',
     name: 'AdminDashboard',
     component: AdminDashboard,
-    meta: { requiresAuth: true, role: 'admin' }, // Solo Admins
+    meta: { requiresAuth: true, role: 'admin' },
   },
   {
     path: '/user-dashboard',
     name: 'UserDashboard',
     component: UserDashboard,
-    meta: { requiresAuth: true, role: 'user' }, // Solo Users
+    meta: { requiresAuth: true, role: 'user' },
   },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: NotFound,
-  },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 
-// Crear router
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
 
-// Protecciones globales (Guardias)
+// Middleware Global
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore()
 
+  // 1. Proteger rutas privadas
   if (to.meta.requiresAuth) {
     if (!authStore.isAuthenticated) {
       next({ name: 'Login' })
@@ -69,6 +53,19 @@ router.beforeEach((to, from, next) => {
       }
       return
     }
+  }
+
+  // 2. Proteger rutas públicas para logueados
+  const publicRoutes = ['Home', 'About', 'Login']
+  if (authStore.isAuthenticated && publicRoutes.includes(to.name as string)) {
+    if (authStore.role === 'admin') {
+      next({ name: 'AdminDashboard' })
+    } else if (authStore.role === 'user') {
+      next({ name: 'UserDashboard' })
+    } else {
+      next({ name: 'Home' })
+    }
+    return
   }
 
   next()
